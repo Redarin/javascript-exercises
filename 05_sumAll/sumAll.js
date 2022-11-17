816ede04d756b8066203ace4b20a2478067a031d
Redarin <GeorgeKsanthopoulos@gmail.com>
2022-11-17 10:40:27 +0200

    sumAll 6 of 6 passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if (x<0 || y<0) {
+        return 'ERROR'
+    }
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return 'ERROR'
+    }
+    let list = [];
+    if (x < y) {
+        for (let i=x; i<=y; i++) {
+            list.push(i);
+        }
+    } else {
+        for (let i=x; i>=y; i--) {
+            list.push(i);
+        }
+    }
+    let intialValue = 0;
+    let sum = list.reduce((previousValue, currentValue) => previousValue +currentValue,intialValue);
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
